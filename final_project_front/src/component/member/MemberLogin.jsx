5f797e3ccee157e5675dfb71b9dda69c354416d9
seojm0629 <seojm0629@gmail.com>
2025-09-30 18:11:50 +0900

    MemberLogin only HTML

--- final_project_front/src/component/member/MemberLogin.jsx
+++ final_project_front/src/component/member/MemberLogin.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import "./member.css";
 
 const MemberLogin = () => {
     return(
         <section className="section login-wrap">
-            <div className="login-page-title">로그인</div>
+            <div className="login-page-title">
+                <img src="/image/final_logo.png" />
+            </div>
             <form onSubmit={(e)=>{
                 e.preventDefault;
             }}>
@@ -24,6 +27,13 @@ const MemberLogin = () => {
                         <div className="login-input-item">
                             <input type="text" name="memberPw" id="memberPw" />
                         </div>
+                    </div>
+                    <div className="login-find">
+                        <Link to="#">아이디 찾기</Link>
+                        
+                        <Link to="#">비밀번호 찾기</Link>
+                        
+                        <Link to="#">회원가입</Link>
                     </div>
                 
 
