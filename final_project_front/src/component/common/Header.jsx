11ac2d29d8cc4a34b1b6d20036a9c8e9c4e3f4be
Lee19941226 <skjb6996@naver.com>
2025-10-01 18:04:41 +0900

    initial commit

---
+++ final_project_front/src/component/common/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <section className="section">
+        <Link to="/">
+          <img src="/image/final_logo.png" />
+        </Link>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
