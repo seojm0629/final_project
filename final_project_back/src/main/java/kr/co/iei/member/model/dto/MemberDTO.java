000d9365bc2bad397320c29f5f4f9f962bf6ae00
seojm0629 <seojm0629@gmail.com>
2025-10-11 03:34:45 +0900

    feat : Member 회원 정보에 대한 패키지 및 클래스 생성

---
+++ final_project_back/src/main/java/kr/co/iei/member/model/dto/MemberDTO.java
@@ -0,0 +1,26 @@
+package kr.co.iei.member.model.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="member")
+public class MemberDTO {
+	
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberNickname;
+	private String memberPhone;
+	private String memberEmail;
+	private int memberGender;
+	private String memberBirth;
+	private String memberDate;
+	private int memberType;
+	private String memberBanType;
+}
