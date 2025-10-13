42dc36939f0a135c287c9468cbd66cbff53c0c7c
Lee19941226 <skjb6996@naver.com>
2025-10-13 19:32:32 +0900

    Test : commit

--- final_project_back/src/main/java/kr/co/iei/freeboard/model/dto/FreeBoardDTO.java
+++ final_project_back/src/main/java/kr/co/iei/freeboard/model/dto/FreeBoardDTO.java
@@ -5,11 +5,12 @@ import org.apache.ibatis.type.Alias;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/*
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Alias(value = "freeBoard")
 public class FreeBoardDTO {
-
+	
 }
+*/
