d91c294c8cec073fcd9037538848d7bcaeacd046
young <>
2025-10-14 18:35:57 +0900

    feat : 중고거래게시판 상세페이지 2차 수정

--- final_project_front/src/component/trade_board/TradeBoardView.jsx
+++ final_project_front/src/component/trade_board/TradeBoardView.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const TradeBoardView = () => {
   const params = useParams();
   const tradeBoardNo = params.tradeBoardNo;
   const [tradeBoard, setTradeBoard] = useState(null);
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACK_SERVER}/tradeBoard/${tradeBoardNo}`)
@@ -17,6 +19,32 @@ const TradeBoardView = () => {
         console.log(err);
       });
   }, []);
+  const deleteBoard = () => {
+    Swal.fire({
+      title: "게시글 삭제",
+      text: "게시글을 삭제하시겠습니까?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "삭제하기",
+      cancelButtonText: "취소",
+    }).then((select) => {
+      if (select.isConfirmed) {
+        axios
+          .delete(
+            `${import.meta.env.VITE_BACK_SERVER}/tradeBoard/${tradeBoardNo}`
+          )
+          .then((res) => {
+            console.log(res);
+            if (res.data === 1) {
+              navigate("/tradeBoard/list");
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+  };
   return (
     <section className="section tradeBoard-view-wrap">
       <div className="page-title">게시글</div>
