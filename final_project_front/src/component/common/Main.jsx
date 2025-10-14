2b383955fae43f088a0668b347e3d6d1404d9e5c
seojm0629 <seojm0629@gmail.com>
2025-10-14 21:02:38 +0900

    feat : 메인페이지 재생성(기존 메인페이지 삭제 및 수정 메인페이지 적용)

--- final_project_front/src/component/common/Main.jsx
+++ final_project_front/src/component/common/Main.jsx
@@ -1,113 +1,148 @@
-import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Main = () => {
-
-    const [activeTab, setActiveTab] = useState("transaction")
+    //login 정보 가져와야함
+    
+    const navigate = useNavigate();
+    const toLogin = () => {
+        navigate("/member/login");
+    }
     return(
         <section className="section main-page">
             <div className="main-wrap">
-                <div className="main-tab-board">
-                    <div className="main-board">
-                        <div className="board-image">
-                            <img src="/image/transaction.png" />
+
+                <div className="list-box">
+                    <div className="main-board-list">
+                        {/* 중고거래 게시판, 자유게시판 */}
+                        <div className="transaction-board first">
+                            <div className="main-board-header">
+                                <h4>중고거래 게시판</h4>
+                            </div>
+                            <div className="main-board-content">
+                                <div className="transaction-list-left">
+                                    <div>제목</div>
+                                </div>
+                                <div className="transaction-list-right">
+                                    <div>익명</div>
+                                    <div>1시간 전</div>
+                                    <div>좋아요</div>
+                                </div>
+                            </div>
+                            <div className="main-board-content">
+                                <div className="transaction-list-left">
+                                    <div>제목</div>
+                                </div>
+                                <div className="transaction-list-right">
+                                    <div>익명</div>
+                                    <div>1시간 전</div>
+                                    <div>좋아요</div>
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div className="board-title">
-                            <span>중고거래 게시판</span>
+
+                        <div className="free-board first">
+                            <div className="main-board-header">
+                                <h4>자유게시판</h4>
+                            </div>
+                            <div className="main-board-content">
+                                <div className="transaction-list-left">
+                                    <div>제목</div>
+                                </div>
+                                <div className="transaction-list-right">
+                                    <div>익명</div>
+                                    <div>1시간 전</div>
+                                    <div>좋아요</div>
+                                </div>
+                            </div>
+                            <div className="main-board-content">
+                                <div className="transaction-list-left">
+                                    <div>제목</div>
+                                </div>
+                                <div className="transaction-list-right">
+                                    <div>익명</div>
+                                    <div>1시간 전</div>
+                                    <div>좋아요</div>
+                                </div>
+                            </div>
                         </div>
+                        
+
                     </div>
 
-                    <div className="main-board">
-                        <div className="board-image">
-                            <img src="/image/work-outline.png" />
+                    <div className="main-board-list">
+                        {/* 스포츠 게시판, 게임 게시판 */}
+                        <div className="sports-board first">
+                            <div className="pre-board-header">
+                                <h4>스포츠 게시판</h4>
+                            </div>
+                            <div className="pre-board-content">
+                                <h1>준비중</h1>
+                            </div>
+                            
+                            
                         </div>
-                        <div className="board-title">
-                            <span>취업 게시판</span>
+
+                        <div className="game-board first">
+                            <div className="pre-board-header">
+                                <h4>게임 게시판</h4>
+                            </div>
+                            <div className="pre-board-content">
+                                <h1>준비중</h1>
+                            </div>
                         </div>
+
                     </div>
 
-                    <div className="main-board">
-                        <div className="board-image">
-                            <img src="/image/board-line.png" />
+                    <div className="main-board-list">
+                        {/* 연예 게시판, 경제 게시판 */}
+                        <div className="sports-board first">
+                            <div className="pre-board-header">
+                                <h4>연예 게시판</h4>
+                            </div>
+                            <div className="pre-board-content">
+                                <h1>준비중</h1>
+                            </div>
+                            
+                            
                         </div>
-                        <div className="board-title">
-                            <span>자유 게시판</span>
+
+                        <div className="game-board first">
+                            <div className="pre-board-header">
+                                <h4>경제 게시판</h4>
+                            </div>
+                            <div className="pre-board-content">
+                                <h1>준비중</h1>
+                            </div>
                         </div>
+
                     </div>
                 </div>
 
-                <div className="main-board-list">
-                    <div className="board-tab">
-                        
-                        <div onClick={()=>{
-                            setActiveTab("transaction");
-                        }}
-
-                        style={{ backgroundColor: activeTab === "transaction" ? "white" : "#2F4E70", 
-                                color : activeTab === "transaction" ? "black" : "white",
-                                borderBottom : activeTab === "transaction" ? "none" : activeTab !== "transaction" && "1px solid black" }}
-                        >중고거래 게시판 인기글</div>
-                        <div onClick={()=>{
-                            setActiveTab("job");
-                        }}
-                        style={{ backgroundColor: activeTab === "job" ? "white" : "#2F4E70", 
-                                color : activeTab === "job" ? "black" : "white",
-                                borderBottom : activeTab === "job" ? "none" : activeTab !== "job" && "1px solid black" 
-
-                        }}
-                        >취업 게시판 인기글</div>
-
-                        <div onClick={()=>{
-                            setActiveTab("free");
-                        }}
-                        style={{ backgroundColor: activeTab === "free" ? "white" : "#2F4E70", 
-                                color : activeTab === "free" ? "black" : "white",
-                                borderBottom : activeTab === "free" ? "none" : activeTab !== "free" && "1px solid black"  }}
-                        >자유 게시판 인기글</div>
+                
+                
+                <div className="main-login-box">
+                    <div className="main-login">
+                        <div className="login-btn">
+                            <button type="submit" onClick={toLogin}>로그인 하기</button>
+                        </div>
+                        <div className="login-find">
+                            <Link to="#">아이디 찾기</Link>
+                            <Link to="#">비밀번호 찾기</Link>
+                            <Link to="#">회원가입</Link>
+                        </div>
                     </div>
 
-
-
-                    <div className="board-list">
-                        {activeTab === "transaction" && 
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td style={{width: "10%"}}>1111</td>
-                                    <td style={{width: "30%"}}>제목</td>
-                                    <td style={{width: "10%"}}>좋아요</td>
-                                    <td style={{width: "15%"}}>익명</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        }
-                        {activeTab === "job" && <table>
-                            <tbody>
-                                <tr>
-                                    <td style={{width: "20%"}}>222</td>
-                                    <td style={{width: "30%"}}>제목</td>
-                                    <td style={{width: "20%"}}>좋아요</td>
-                                    <td style={{width: "30%"}}>익명</td>
-                                </tr>
-                            </tbody>
-                        </table>}
-
-                        {activeTab === "free" && <table>
-                            <tbody>
-                                <tr>
-                                    <td style={{width: "10%"}}>333</td>
-                                    <td style={{width: "30%"}}>제목</td>
-                                    <td style={{width: "10%"}}>좋아요</td>
-                                    <td style={{width: "15%"}}>익명</td>
-                                </tr>
-                            </tbody>
-                        </table>}    
-                    </div>    
+                    <div className="main-topic">
+                        <h3>today's topic</h3>
+                        <div className="topic-list">
+                            <h1>준비중</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-
     )
-
 }
 
 export default Main;
