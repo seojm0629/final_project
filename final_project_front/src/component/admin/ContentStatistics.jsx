c2643c8ea806a0c51be95e9f6b1ebeb31caffed4
jwlee <ljw155070@gmail.com>
2025-10-17 17:02:39 +0900

    feat : 차트 셋 컴포넌트 분리

--- final_project_front/src/component/admin/ContentStatistics.jsx
+++ final_project_front/src/component/admin/ContentStatistics.jsx
@@ -4,125 +4,119 @@ import "./contentStatistics.css";
 import { Bar } from "react-chartjs-2";
 
 const ContentStatistics = () => {
-  const MonthChart = () => {
-    const data = {
-      labels: [
-        "1월",
-        "2월",
-        "3월",
-        "4월",
-        "5월",
-        "6월",
-        "7월",
-        "8월",
-        "9월",
-        "10월",
-        "11월",
-        "12월",
-      ],
-      datasets: [
-        {
-          label: "가입자 수",
-          data: [12, 19, 3, 5, 2, 3, 8, 20, 1, 6, 5, 70],
-          borderWidth: 1,
-          backgroundColor: "rgba(54,162,235,0.5)",
+  const InsertMember = () => {
+    const MonthChart = () => {
+      const data = {
+        labels: [
+          "1월",
+          "2월",
+          "3월",
+          "4월",
+          "5월",
+          "6월",
+          "7월",
+          "8월",
+          "9월",
+          "10월",
+          "11월",
+          "12월",
+        ],
+        datasets: [
+          {
+            label: "가입자 수",
+            data: [12, 19, 3, 5, 2, 3, 8, 20, 1, 6, 5, 70],
+            borderWidth: 1,
+            backgroundColor: "rgba(54,162,235,0.5)",
+          },
+        ],
+      };
+
+      const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: { display: true, text: "월별 가입자" },
+          legend: { position: "top" },
         },
-      ],
+        scales: { y: { beginAtZero: true } },
+      };
+
+      return (
+        <div>
+          <Bar
+            data={data}
+            options={options}
+            style={{ position: "relative", height: "300px" }}
+          />
+        </div>
+      );
     };
+    const DaysChart = () => {
+      const data = {
+        labels: [
+          "1일",
+          "2일",
+          "3일",
+          "4일",
+          "5일",
+          "6일",
+          "7일",
+          "8일",
+          "9일",
+          "10일",
+          "11일",
+          "12일",
+          "13일",
+          "14일",
+          "15일",
+          "16일",
+          "17일",
+          "18일",
+          "19일",
+          "20일",
+          "21일",
+          "22일",
+          "23일",
+          "24일",
+          "25일",
+          "26일",
+          "27일",
+          "28일",
+          "29일",
+        ],
+        datasets: [
+          {
+            label: "가입자 수",
+            data: [12, 19, 3, 5, 2, 3, 8, 20, 1, 6, 5, 70],
+            borderWidth: 1,
+            backgroundColor: "rgba(54,162,235,0.5)",
+          },
+        ],
+      };
 
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        title: { display: true, text: "월별 가입자" },
-        legend: { position: "top" },
-      },
-      scales: { y: { beginAtZero: true } },
-    };
-
-    return (
-      <div>
-        <Bar
-          data={data}
-          options={options}
-          style={{ position: "relative", height: "200px", width: "100px" }}
-        />
-      </div>
-    );
-  };
-
-  const DaysChart = () => {
-    const data = {
-      labels: [
-        "1일",
-        "2일",
-        "3일",
-        "4일",
-        "5일",
-        "6일",
-        "7일",
-        "8일",
-        "9일",
-        "10일",
-        "11일",
-        "12일",
-        "13일",
-        "14일",
-        "15일",
-        "16일",
-        "17일",
-        "18일",
-        "19일",
-        "20일",
-        "21일",
-        "22일",
-        "23일",
-        "24일",
-        "25일",
-        "26일",
-        "27일",
-        "28일",
-        "29일",
-      ],
-      datasets: [
-        {
-          label: "가입자 수",
-          data: [12, 19, 3, 5, 2, 3, 8, 20, 1, 6, 5, 70],
-          borderWidth: 1,
-          backgroundColor: "rgba(54,162,235,0.5)",
+      const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: { display: true, text: "월별 가입자" },
+          legend: { position: "top" },
         },
-      ],
-    };
+        scales: { y: { beginAtZero: true } },
+      };
 
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        title: { display: true, text: "월별 가입자" },
-        legend: { position: "top" },
-      },
-      scales: { y: { beginAtZero: true } },
+      return (
+        <div>
+          <Bar
+            data={data}
+            options={options}
+            style={{ position: "relative", height: "300px" }}
+          />
+        </div>
+      );
     };
-
     return (
-      <div>
-        <Bar
-          data={data}
-          options={options}
-          style={{ position: "relative", height: "200px", width: "100px" }}
-        />
-      </div>
-    );
-  };
-  const [chartToggle, setChartToggle] = useState("month");
-  return (
-    <div className="admin-content-wrap">
-      <div className="content-head">
-        <div className="title m">통계 페이지</div>
-        <div className="title s">사이트 이용 지표</div>
-      </div>
-      <div className="content-body">
-        <div className="firstChartBox">
+      <div className="wrap-1">
+        <div className="chartBox">
           <button
             onClick={() => {
               setChartToggle("month");
@@ -141,6 +135,32 @@ const ContentStatistics = () => {
         <div>
           {chartToggle === "month" && MonthChart()}
           {chartToggle === "days" && DaysChart()}
+        </div>
+      </div>
+    );
+  };
+
+  const [chartToggle, setChartToggle] = useState("month");
+  return (
+    <div className="admin-right">
+      <div className="admin-content-wrap">
+        <div className="content-head">
+          <div className="title m">통계 페이지</div>
+          <div className="title s">사이트 이용 지표</div>
+        </div>
+        <div className="chart-content-body">
+          <div>
+            <InsertMember />
+            <InsertMember />
+          </div>
+          <div>
+            <InsertMember />
+            <InsertMember />
+          </div>
+          <div>
+            <InsertMember />
+            <InsertMember />
+          </div>
         </div>
       </div>
     </div>
