0c68f4ccee01b2e520b54a025a36ded19d3ea6c6
wjsgusdn <wjsgusdn96tp@gmail.com>
2025-10-29 16:38:18 +0900

    feat : 자기가 투표한 게시글 표시쿼리 출력완료

--- final_project_back/src/main/java/kr/co/iei/vote/model/service/VoteService.java
+++ final_project_back/src/main/java/kr/co/iei/vote/model/service/VoteService.java
@@ -41,8 +41,9 @@ public class VoteService {
 	}
 
 	@Transactional
-	public HashMap<String, Object> voteList(int startRow, int endRow, int sideBtnCount, int order) {
+	public HashMap<String, Object> voteList(int startRow, int endRow, int sideBtnCount, int order, int memberNo) {
 		System.out.println("서비스에서 오더값 확인" + order);
+		System.out.println("서비스에서 멤버번호 확인" + memberNo);
 		int voteCheck = voteDao.updateVoteCheck(); // 종료일 비교해서 0 에서 1로 바꾸기
 		
 		HashMap<String, Object> voteList = new HashMap<String,Object>();
@@ -50,7 +51,7 @@ public class VoteService {
 		voteList.put("endRow", endRow);
 		voteList.put("sideBtnCount", sideBtnCount);
 		voteList.put("order", order);
-
+		voteList.put("memberNo", memberNo);
 		
 		List<VoteDTO> selectVoteList = voteDao.selectVoteList(voteList);// 리스트 출력
 		int totalListCount = voteDao.totalListCount(voteList); // 리스트 갯수의 총 합
