d94d1cfb32b6d461dabddeaa3db49aae7f356f4a
wjsgusdn <wjsgusdn96tp@gmail.com>
2025-11-02 11:44:22 +0900

    chore : 오류수정

--- final_project_back/src/main/java/kr/co/iei/vote/model/dao/VoteDao.java
+++ final_project_back/src/main/java/kr/co/iei/vote/model/dao/VoteDao.java
@@ -60,18 +60,16 @@ public interface VoteDao {
 
 	int deleteComment(int voteCommentNo);
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> 5a901fbb6b1e364398940064eefe17c1989027c4
+
+
 	int checkDuplicateCommentReport(HashMap<String, Object> reportData);
 
 	int insertCommentReport(HashMap<String, Object> reportData);
 
-<<<<<<< HEAD
-=======
+
 	int checkVoteLike(HashMap<String, Object> likeData);
 
 	int insertVoteLike(HashMap<String, Object> likeData);
@@ -81,7 +79,7 @@ public interface VoteDao {
 	int checkVoteReport(HashMap<String, Object> reportData);
 
 	int insertVoteReport(HashMap<String, Object> reportData);
->>>>>>> 5a901fbb6b1e364398940064eefe17c1989027c4
+
 
 	
 }
