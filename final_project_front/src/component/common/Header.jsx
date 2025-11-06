50b0168756e9d4a012635643f2e127d90ebc112c
ljw155070-a11y <ljw155070@gmail.com>
2025-11-07 01:42:11 +0900

    feat : 상단 탭 추가 (SSR 개발 리스트)

--- final_project_front/src/component/common/Header.jsx
+++ final_project_front/src/component/common/Header.jsx
@@ -53,7 +53,7 @@ const MainNavi = () => {
           </button>
         </li>
         <li className="navi-item vote">
-          <a href="/notice/list">공지사항</a>
+          <a href="/notice/list">공지사항(관리자 전용)</a>
         </li>
         <li className="navi-item free">
           <button className="board-btn">
@@ -62,6 +62,12 @@ const MainNavi = () => {
         </li>
         <li className="navi-item vote">
           <Link to="/vote/list">투표게시판</Link>
+        </li>
+        <li className="navi-item vote">
+          <a href="/notice/list">회사 소개</a>
+        </li>
+        <li className="navi-item vote">
+          <a href="/notice/list">서비스 소개</a>
         </li>
       </ul>
     </nav>
